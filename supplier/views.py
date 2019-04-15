e576adaaf0c43f362a99d3950c8a42f85038d78e
ThaleS_CS <th4les.castro@gmail.com>
2019-04-14 21:43:52 -0300

    Django com teste.

---
+++ supplier/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Supplier
+
+# Create your views here.
+
+
+
+def supplier_list(request):
+    #suppliers = Supplier.objects.all() 
+    suppliers = Supplier.objects.exclude(phone__isnull=True).exclude(phone__exact='')
+    context = {
+        'supplier_list': suppliers
+    }
+    return render(request, 'supplier/list.html', context)
